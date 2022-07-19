33b30f4f7f05c5e29a548bc7e3d26678a306b00a
Animesh <animeshjain0706@gmail.com>
2022-07-19 18:13:37 +0530

    modified:   client/package.json 	modified:   index.js 	modified:   logfile.log 	modified:   package-lock.json 	modified:   package.json 	modified:   startup/db.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const winston = require("winston");
 const express = require("express");
 const config = require("config");
 const app = express();
+const path = require("path");
+require("dotenv").config();
 var cors = require("cors");
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
@@ -11,6 +16,9 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
--- startup/db.js
+++ startup/db.js
@@ -6,6 +6,6 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
 module.exports = function () {
-  const db = config.get("db");
+  const db = process.env.db || config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
